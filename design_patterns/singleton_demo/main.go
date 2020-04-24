b6302429342a083a9382df8ce3845ad433d83119
Wu Huijie <hjwu85@gmail.com>
2020-04-24 21:44:26 +0800

    update demo logic

--- design_patterns/singleton_demo/main.go
+++ design_patterns/singleton_demo/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/edgejay/go-data-algorithms/design_patterns/singleton"
 )
+
+var lock sync.Mutex
+
+func addOneInThread(ch chan<- int) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	singletonCounter := singleton.GetInstance()
+
+	singletonCounter.AddOne()
+	ch <- singletonCounter.Value()
+}
 
 func main() {
 	singletonCounter := singleton.GetInstance()
@@ -14,4 +27,14 @@ func main() {
 
 	anotherCounter := singleton.GetInstance()
 	fmt.Println("anotherCounter.Value() : ", anotherCounter.Value())
+
+	ch := make(chan int)
+
+	go addOneInThread(ch)
+	go addOneInThread(ch)
+	go addOneInThread(ch)
+
+	fmt.Printf("counter value: %d\n", <-ch)
+	fmt.Printf("counter value: %d\n", <-ch)
+	fmt.Printf("counter value: %d\n", <-ch)
 }
